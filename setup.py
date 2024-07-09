5599f6a7e77185e095db78e0073996eae4fc60ab
Luis Martinez <lcmartinezl05@gmail.com>
2024-07-08 22:10:45 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="alegra",
+    packages=find_packages(include=["alegra", "alegra.*"]),
+    version="0.1.0",
+    description="Python SDK for Alegra API",
+    author="Luis Martinez",
+    install_requires=[
+        "pydantic[email]==2.8.2",
+        "requests==2.32.3",
+    ],
+    test_suite="tests",
+    python_requires=">=3.6",
+)
