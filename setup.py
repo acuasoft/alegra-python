ad7095796ad6cdb0da91d76c79a93826766d6a27
Luis Martínez <lcmartinezl05@gmail.com>
2025-01-24 08:43:27 -0500

    GitHub actions job (#7)
    * Add github workflow
    
    * Implement CI job to publish the package automaticaly

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="alegra",
+    name="alegra-e-provider",
     packages=find_packages(include=["alegra", "alegra.*"]),
-    version="0.1.0",
-    description="Python SDK for Alegra API",
+    version="0.0.1",
+    description="Alegra E-Provider, Python Wrapper for Alegra Electronic Provider API",
+    long_description=open("README.md").read(),
     author="Luis Martinez",
     install_requires=[
         "pydantic[email]==2.8.2",
